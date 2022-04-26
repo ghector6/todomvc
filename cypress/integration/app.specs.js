712d2b9710f14a9ccc0d3ceee66e14fabfdca545
ghector6 <ghectormus@gmail.com>
2022-04-26 16:02:41 -0500

    Paso 4: Agregando un segundo todo para que la prueba truene

--- cypress/integration/app.specs.js
+++ cypress/integration/app.specs.js
@@ -2,6 +2,7 @@ describe("React TodoMVC", () =>{
     it("adds a single todo", () => {
         cy.visit("http://localhost:8888")
         cy.get(".new-todo").type("Buy Milk{enter}")
+        cy.get(".new-todo").type("Learn Cypress{enter}")
         cy.get(".todo-list li").should("have.length", 1)
     })
 })
