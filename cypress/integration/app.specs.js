a4be60a1f671f0b3cf369dc3547b4f16dd5d641a
ghector6 <ghectormus@gmail.com>
2022-04-27 11:09:29 -0500

    Paso 8: Agregando la función beforeEach() para visit()

--- cypress/integration/app.specs.js
+++ cypress/integration/app.specs.js
@@ -1,12 +1,17 @@
 describe("React TodoMVC", () =>{
-    it("adds a single todo", () => {
+    beforeEach(()=>{
         cy.visit("http://localhost:8888")
+    })
+
+
+    it("adds a single todo", () => {
+        
         cy.get(".new-todo").type("Buy Milk{enter}").type("Learn Cypress{enter}")
         
         cy.get(".todo-list li").should("have.length", 2)
     })
     it("add three todos", () => {
-        
+
     })
 
 
