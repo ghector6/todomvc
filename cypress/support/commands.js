30689b06204536e2e08fd9e4a11dd6892a83ac80
ghector6 <ghectormus@gmail.com>
2022-04-29 10:43:05 -0500

    Paso 12: Agregando Cypress.Commands

---
+++ cypress/support/commands.js
@@ -0,0 +1,8 @@
+Cypress.Commands.add("createDefaultTodos", () => {
+    
+    const TODO_ITEM_ONE = 'Buy Milk'
+
+    const TODO_ITEM_TWO = 'Practice JavaScript'
+
+    const TODO_ITEM_THREE = 'Get Files Done'
+})
