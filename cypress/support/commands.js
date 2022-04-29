3b13186f02562756fe43b89c1093aa694ab3309e
ghector6 <ghectormus@gmail.com>
2022-04-29 11:06:35 -0500

    Paso 13: Agregando la habilidad de hacer todos en archivo commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -5,4 +5,12 @@ Cypress.Commands.add("createDefaultTodos", () => {
     const TODO_ITEM_TWO = 'Practice JavaScript'
 
     const TODO_ITEM_THREE = 'Get Files Done'
+
+    cy.get(".new-todo")
+    
+        .type(`${TODO_ITEM_ONE}{enter}`)
+
+        .type(`${TODO_ITEM_TWO}{enter}`)
+
+        .type(`${TODO_ITEM_THREE}{enter}`)
 })
